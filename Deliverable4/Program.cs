f7028bee2d6af4c6337d45446688d605464e25e2
alexs <alexs@ALEJANDROSPC>
2023-02-09 13:19:10 -0500

    Add project files.

---
+++ Deliverable4/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Author: Alejandro Sanchez
+Date: 2/9/2023
+Description: C# Fibonacci array sequence
+ */
+using System;
+
+namespace Deliverable4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            int n = 25;
+
+            int[] fibonacci = new int[n];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 2; i < n; i++)
+            {    
+                fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Fibonacci number (" + i + ") = " + fibonacci[i]);
+            }
+        }
+    }
+}
